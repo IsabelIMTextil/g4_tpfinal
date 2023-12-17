e7e866ecb61394ffd9dda52066f04f15138ecb1b
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2023-12-17 19:51:46 -0300

    Update main.php

--- pages/main.php
+++ pages/main.php
@@ -7,7 +7,7 @@
             <div id="carouselExample" class="carousel slide">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="imagenes/wichí.jpg" class="d-block w-100" alt="Textiles Wichí" style="height:300px;">
+                    <img src="imagenes/wichi.jpg" class="d-block w-100" alt="Textiles Wichí" style="height:300px;">
                     <div class="carousel-caption text-right caption-center">
                       <h2 class="bg-dark">Textiles tradicionales del territorio argentino</h2>
                       <p>Charlas sobre cultura, lengua y cosmovisión de los pueblos originarios de Argentina. Talleres sobre técnicas textiles Wichí, Mapuche y del NOA.</p>
